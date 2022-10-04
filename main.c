ede0ad8d127d1e8e77a4eb66b794969aa7a13f39
mjiw <113112060+mjiw@users.noreply.github.com>
2022-10-04 17:41:33 +0900

    05

--- main.c
+++ main.c
@@ -4,17 +4,35 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sum=0;
-	int num=0;
-	int i;
+	int num1=0;
+	int num2=0;
+	char c;
+	int result=0;
 	
-	printf("input a number:");
-	scanf("%d",&num);
 	
-	for(i=1;i<=num;i++){
-		sum=sum+i;
-	} 
+	printf("enter the calculation:");
+	scanf("%d %c %d",&num1,&c,&num2);
 	
-	printf("The result is %d",sum);
+	
+	switch(c){
+		case '+':
+			result=num1+num2;
+		break;
+		
+		case '-':
+			result=num1-num2;
+		break;
+		
+		case '*':
+			result=num1*num2;
+		break;
+		
+		case '/':
+			result=num1/num2;
+		break;
+	}
+	
+	printf("=%d",result);
+	
 	return 0;
 }
